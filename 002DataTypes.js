0ce8d08187b30d39b67c4062a15a7b5f4be75f8b
haruna_hannah <harunahannah04@gmail.com>
2025-06-12 18:47:55 +0100

    002DataTypes.js file created

---
+++ 002DataTypes.js
@@ -0,0 +1,55 @@
+let firstname ="Hannah";
+let greeting ='Hello!';
+console.log(firstname);
+console.log(typeof firstname);
+console.log(greeting);
+console.log(typeof greeting);
+
+let age = 30;
+let height = 6.8;
+console.log(age);
+console.log(typeof age);
+console.log(height);
+console.log(typeof height)
+
+let isfemale = true;
+let hasvacated = false;
+console.log(isfemale);
+console.log(typeof isfemale);
+console.log(hasvacated);
+console.log(typeof hasvacated)
+
+let MiddleName;
+let State = undefined;
+console.log(MiddleName);
+console.log(typeof MiddleName);
+console.log(State);
+console.log(typeof State);
+
+let qualification =null;
+let position =null;
+console.log(qualification);
+console.log(typeof qualification);
+console.log(position);
+console.log(typeof position);
+
+let bigNum =123456789n;
+let anotherBig = BigInt(1234567890123456789);
+console.log(bigNum);
+console.log(typeof bigNum);
+console.log(anotherBig);
+console.log(typeof anotherBig);
+
+let id =Symbol("id");
+let anotherid =Symbol("id");
+console.log(id);
+console.log(typeof id);
+console.log(anotherid);
+console.log(typeof anotherid);
+
+let person = {name: "Hannah", age: 30, complexion: 'Dark', height: 5.8};
+let scores = [67, 98, 59, 90];
+console.log(person);
+console.log(typeof person);
+console.log(scores);
+console.log(typeof scores);
