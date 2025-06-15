343dba2988e8f6a960f486bd9bc9e149d830c6ee
haruna_hannah <harunahannah04@gmail.com>
2025-06-16 00:06:24 +0100

    006LogicalOperators.js file created

---
+++ 006LogicalOperators.js
@@ -0,0 +1,9 @@
+let x = 18;
+let y = 8; 
+let z = 9;
+let data = x > y;
+let data1 = x > y && x > z;
+let data2 = x < y && x < z;
+let data3 = x > y || x > z;
+let n = !data3;
+console.log(data3, n);
