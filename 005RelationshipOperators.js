bdc08cb14f826b1c8550fc74356f6ac2664af169
haruna_hannah <harunahannah04@gmail.com>
2025-06-15 23:32:44 +0100

    005RelationshipOperators.js file created

---
+++ 005RelationshipOperators.js
@@ -0,0 +1,53 @@
+//console.log(5 > 6);
+//console.log(9 > 5);
+let data = 9 > 5;
+console.log(data);
+let data1 = 9 <= 5;
+console.log(data1);
+
+let x = 5;
+let y = 5;
+let data2 = x >= y;
+console.log(data2);
+
+let a = "car";
+let b = "bike";
+let data3 = a > b;
+console.log(data3);
+
+let c = "6";
+let d = 3;
+let data4 = c > d;
+console.log(data4);
+
+let e = 5;
+let f = 5;
+let data5 = e == f;
+console.log(data5);
+
+let g = "5";
+let h = 5;
+let data6 = g == h;
+console.log(data6);
+
+let i = "5";
+let j = 5;
+let data7 = i === j;
+console.log(data7);
+
+let k = '';
+let l = false;
+let data8 = k == l;
+console.log(data8);
+
+let m = '';
+let n = false;
+let data9 = m === n;
+console.log(data9);
+
+
+
+
+
+
+
