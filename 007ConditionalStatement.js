0742657015b24628535d54b35f4cb164f908e7f8
haruna_hannah <harunahannah04@gmail.com>
2025-06-18 00:44:56 +0100

    007ConditionalStatement.js file created

---
+++ 007ConditionalStatement.js
@@ -0,0 +1,24 @@
+let num1 = 9;
+let num2 = 5;
+let num3 = 7;
+
+if(num1 > num2 && num1 > num3){
+    console.log("num1 is greater");
+    }
+
+else if (num2 > num1 && num2 > num3){
+    console.log("num2 is greater");
+   } 
+
+else {
+    console.log("num3 is greater");
+    
+}
+    
+console.log("Bye...");
+
+    
+
+
+
+    
