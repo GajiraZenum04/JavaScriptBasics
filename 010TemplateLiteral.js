527a2d12c659290e4980a8c85d0955e70b35f4c1
haruna_hannah <harunahannah04@gmail.com>
2025-06-19 09:58:53 +0100

    010TemplateLiteral.js file created

---
+++ 010TemplateLiteral.js
@@ -0,0 +1,17 @@
+let num1 = 9;
+let num2 = 5;
+let result = num1 + num2;
+//console.log(result);
+//console.log("the addition of 9 and 5 is 14");
+//console.log("the addition of " + num1 + " and " + num2 + " is " + result);
+console.log(`the addition of ${num1} and ${num2} is ${result}`);
+console.log(`my name is
+haruna hannah`);
+
+
+
+
+
+
+
+
