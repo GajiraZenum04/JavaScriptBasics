46d9396a547458ff2cd2626928091028c013f47e
haruna_hannah <harunahannah04@gmail.com>
2025-06-24 00:09:42 +0100

    015ComplexObjects.js file created

---
+++ 015ComplexObjects.js
@@ -0,0 +1,17 @@
+
+
+let alien = {
+    name: 'Hannah',
+    tech: 'JS',
+    laptop: {
+        cpu : 'i7',
+        ram : 4,
+        brand1 : 'Asus'
+    }
+}
+
+delete alien.laptop
+console.log(alien);
+
+
+
