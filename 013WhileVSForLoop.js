31af640b06b50cd6c6032a6eb16eca19416d2ebe
haruna_hannah <harunahannah04@gmail.com>
2025-06-23 23:27:00 +0100

    013WhileVSForLoop.js file created

---
+++ 013WhileVSForLoop.js
@@ -0,0 +1,15 @@
+
+
+//for (let i=1;i<=100;i++) 
+//{
+//    if (i%3===0) 
+//        console.log(i);          
+//}
+
+let num = 123456
+
+while (num>0) 
+{
+    console.log(num%10);
+    num = parseInt(num / 10) 
+}
